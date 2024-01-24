e40d256ff8f3debb03efd8c8426440b6ee5aacf4
xVanTuring <xVanTuring@foxmail.com>
2024-01-24 19:15:29 +0800

    replace \r to \n

--- DownloadCFIP.ps1
+++ DownloadCFIP.ps1
@@ -3,9 +3,9 @@ $IPS = Invoke-WebRequest $URL
 
 $Lines = $IPS.Content -split "`n"
 
-$Text = "payload:`r"
+$Text = "payload:`n"
 for ($i = 0; $i -lt $Lines.Count; $i++) {
-    $Text += "  - {0}`r" -f $Lines[$i]
+    $Text += "  - {0}`n" -f $Lines[$i]
 }
 Write-Output $Text > cloudflare.txt
 Copy-Item -Force cloudflare.txt ./ruleset/cfcidr.txt
